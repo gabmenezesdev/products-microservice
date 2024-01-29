a4aa2614fcdae845d9f8476fdfa49d69ebc06719
Gabriel Santos <gabriel.menezes.santos@hotmail.com>
2024-01-28 23:00:41 -0300

    first router on swagger

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,29 @@ import express, { Request, Response, NextFunction } from "express";
 import { router } from "./routes";
 import * as dotenv from "dotenv";
 import * as mongoose from "mongoose";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// DB connection
 mongoose.connect(process.env.DB_URL || "localhost:27017");
 
+// Service validation route
 app.get("/ping", (req, res) => {
   return res.send("Service online!");
 });
 
+// Service routes
 app.use("/api/", router);
 
-// error handling
+// swagger doc
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Error handling
 app.use(function error(
   err: any,
   req: Request,
